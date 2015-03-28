287f2cd68633e6325c48cff73815a18229901912
pol2015 <polettof@yahoo.com>
2015-03-28 16:43:50 -0300

    - working page title- - cleaned code - no comments

--- url_chk/url_chk_4.py
+++ url_chk/url_chk_4.py
@@ -11,52 +11,19 @@ data=csv.reader(in_file)
 out_data=csv.writer(out_file)
 
 soup=[]
-#print soup
-#soup.append(7)
-#soup.append(8)
-#print soup
+
 
 data = [row for row in csv.reader(in_file)  ]
 
 
-#print data[0]
-#print data[1]
-#print data[2]
-#print data[0][0]
-#print data[1][0]
-#print data[2][0]
-#print row[0]
-
-#print data[0][0]
-
-#soup=BeatifulSoup(urllib2,urlopen(data[0][0]))
-#print soup.title.string
 
 row_count = len(data)
 
 for index,item in enumerate(data):
-    #print index+1
-    #print item[0]
+
     soup = BeautifulSoup(urllib2.urlopen(item[0]))
     print soup.title.string
     item[1] = soup.title.string
-
-
-#url = data[0][0]
-#soup = BeautifulSoup(urllib2.urlopen(url))
-#data [0][1] = soup.title.string
-#print data[0][1]
-
-
-#url = data[0][0]
-
-#print data[0][1]
-
-#soup = BeautifulSoup(urllib2.urlopen(url))
-#print soup.title.string
-
-
-
 
 for item in data:
     out_data.writerow(item)
